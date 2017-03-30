dffca7301415105f056189edb527fd6f8d8ac206
My rails project <abhijosh92@gmail.com>
2017-03-30 08:10:50 +0000

    created association between users and articles. added debugger view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(articles_params)
+    @article.user = User.first
     if @article.save
       flash[:success] = "The article was successfully created"
       redirect_to article_path(@article)
